f1a8cb9d4cb18bedcffe558cdb0168836e982627
Ryan Thompson <ryan.thompson@nuand.com>
2023-12-15 17:05:24 -0600

    added hello_libbladeRF crate

---
+++ hello_libbladeRF/src/lib.rs
@@ -0,0 +1,54 @@
+/* Copyright (c) 2023 Nuand LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::ffi::CStr;
+
+    #[test]
+    fn test_libbladeRF_version() {
+        unsafe {
+            let mut version = bladerf_version {
+                major: 0,
+                minor: 0,
+                patch: 0,
+                describe: std::ptr::null(),
+            };
+
+            bladerf_version(&mut version as *mut _);
+
+            let describe_cstr = CStr::from_ptr(version.describe);
+            let describe_str = describe_cstr.to_str().unwrap();
+
+            println!("libbladeRF version: {}.{}.{} - {}",
+                     version.major, version.minor, version.patch, describe_str);
+
+        }
+    }
+}
